91566809e945b0d68c7b37d507e2bed87e663dee
thanhkhiet2008 <lethanhkhietnguyen@gmail.com>
2024-11-12 09:34:40 +0700

    Update themLK.php

--- qliLinhKien/themLK.php
+++ qliLinhKien/themLK.php
@@ -1,14 +1,16 @@
 <?php
 $MaSP= $_POST['MaSP'];
 $TenSP= $_POST['TenSP'];
+$NhanHang= $_POST['NhanHang'];
 $NgayNhap=$_POST['NgayNhap'];
 $NgayXuat= $_POST['NgayXuat'];
 $GiaSP=$_POST['GiaSP'];
+$SoLuong=$_POST['SoLuong'];
 require "database.php";
 $db= new Database();
-$sql="INSERT INTO `linhkien` (`MaSP`, `TenSP`, `NgayNhap`, `NgayXuat`, `GiaSP`) VALUES (?,?,?,?,?);";
+$sql="INSERT INTO `linhkien` (`MaSP`, `TenSP`,`NhanHang`, `NgayNhap`, `NgayXuat`, `GiaSP`,`SoLuong`) VALUES (?,?,?,?,?,?,?);";
 $db->setSQL( $sql);
-$kq=$db->executeSql([$MaSP,$TenSP,$NgayNhap,$NgayXuat,$GiaSP]);
+$kq=$db->executeSql([$MaSP,$TenSP,$NhanHang,$NgayNhap,$NgayXuat,$GiaSP,$SoLuong]);
 if($kq) {echo "thanh cong";}
 ?>
 <a href="index.php"> Trang chủ </a>
